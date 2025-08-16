33a0b0425b5f29c685ea329dfb1b5113a4ed69b7
Math012 <math111.0811@gmail.com>
2025-08-16 00:24:37 -0300

    Adicionando a anotação: @ EnableFeignClients ao OrderServiceApplication

--- src/main/java/com/math012/order_service/OrderServiceApplication.java
+++ src/main/java/com/math012/order_service/OrderServiceApplication.java
@@ -2,7 +2,9 @@ package com.math012.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.*;
 
+@EnableFeignClients
 @SpringBootApplication
 public class OrderServiceApplication {
 
